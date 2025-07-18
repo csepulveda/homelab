d81256edab6630d719477d76dd19e20c98004dad
César Sepúlveda <cesar.sepulveda.b@gmail.com>
2025-07-18 12:41:37 -0400

    test

--- monitoring/kube/run_on_updates.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd monitoring/kube
-rm Chart.lock
-rm -fr charts/*
-helm dependency build
-tar xvfz charts/kube-prometheus-stack-*.tgz -C charts
-rm -fr charts/kube-prometheus-stack-*.tgz
-cd hacks
-bash update-annotation.sh
